591fb83f8017c32b949a480e5863c4ce4a4f9ae1
Sanensi <william.picard@hotmail.com>
2023-12-20 20:51:55 -0500

    feat: react-based checkboard grid POC

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Grid } from "./Grid";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1>Hello World!</h1>
+    <Grid />
   </React.StrictMode>,
 );
