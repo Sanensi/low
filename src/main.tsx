95099754b5cca90d6672d81baa5e29c77cd1de92
Sanensi <william.picard@hotmail.com>
2023-12-21 20:03:16 -0500

    chore: Remove poc

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Grid } from "./Grid";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Grid />
-  </React.StrictMode>,
+  <React.StrictMode>Hello World!</React.StrictMode>,
 );
