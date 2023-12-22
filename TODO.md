2ce0e02c4407cbd489f8e2f066a0831c4e932127
Sanensi <william.picard@hotmail.com>
2023-12-22 14:03:39 -0500

    feat: draw multiple hexes on the screen

--- TODO.md
+++ TODO.md
@@ -3,3 +3,14 @@
 ## Notes
 
 - When using PixiApplication.destroy, the application is broken apart by HMR
+
+### Hexagonal coordinates
+
+We use (q, r, s) with the constraint `q + r + s = 0` for the hexagonal coordinates system.
+
+Rule of thumbs to remember the meaning of each value (flat-top hexes):
+
+- Imagine the hands of a clock
+- `q` becomes positive at 3 o'clock, negative at 9 o'clock
+- `r` becomes positive at 7 o'clock, negative at 1 o'clock
+- `s` becomes positive at 11 o'clock, negative at 5 o'clock
--- src/game/LoW.ts
+++ src/game/LoW.ts
@@ -1,6 +1,21 @@
 import { Container } from "pixi.js";
 import { PixiApplicationBase } from "../lib/PixiApplicationBase";
 import { createHex } from "./HexGraphics";
+import { HexCoordinate, hexToPixel, pixelToHex } from "../lib/HexCoordinate";
+import { Vec2 } from "../lib/Vec2";
+
+const SCALE = Vec2.ONE.scale(100);
+const UNIT_HEX = createHex({ radius: SCALE.x, lineWidth: 10 });
+
+const coordinates = [
+  new HexCoordinate(0, 0, 0),
+  new HexCoordinate(1, -1, 0),
+  new HexCoordinate(1, 0, -1),
+  new HexCoordinate(0, 1, -1),
+  new HexCoordinate(-1, 1, 0),
+  new HexCoordinate(-1, 0, 1),
+  new HexCoordinate(0, -1, 1),
+];
 
 export class LoW extends PixiApplicationBase {
   private map = new Container();
@@ -8,11 +23,23 @@ export class LoW extends PixiApplicationBase {
   constructor(canvas: HTMLCanvasElement) {
     super(canvas, { backgroundColor: "#ffffff", antialias: true });
     this.init();
+
+    this.map.eventMode = "static";
+    this.map.addEventListener("click", (e) => {
+      const p = e.getLocalPosition(this.map);
+      console.log(pixelToHex(new Vec2(p), SCALE));
+    });
   }
 
   protected start(): void {
-    const hex = createHex();
-    this.map.addChild(hex);
+    for (const coord of coordinates) {
+      const p = hexToPixel(coord, SCALE);
+      const hex = UNIT_HEX.clone();
+      hex.position.copyFrom(p);
+      this.map.addChild(hex);
+    }
+
+    this.map.scale.set(0.5);
     this.app.stage.addChild(this.map);
     this.resize();
   }
